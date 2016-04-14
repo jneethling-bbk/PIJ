ebcfad766dec4ea70f06209c38a13b4bd0899930
jneethling-bbk <jneethling.bbk@gmail.com>
2016-04-14 17:38:40 +0100

    Code to check result of code tracing problem

--- Recursion/src/basics/NumberPrinter.java
+++ Recursion/src/basics/NumberPrinter.java
@@ -3,8 +3,17 @@ package basics;
 public class NumberPrinter {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		printNumbers(6);
+	}
+	
+	public static void printNumbers(int n) {
+		if (n <= 0) {
+			return;
+		}
+		System.out.println(n);
+		printNumbers(n-2);
+		printNumbers(n-3);
+		System.out.println(n);
 	}
 
 }
