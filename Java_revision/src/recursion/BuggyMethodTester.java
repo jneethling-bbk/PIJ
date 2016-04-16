4b258ba6268808573803a6e60ab2f459b35c43cb
jneethling-bbk <jneethling.bbk@gmail.com>
2016-04-16 16:06:23 +0100

    Java basic recursion exercises

---
+++ Java_revision/src/recursion/BuggyMethodTester.java
@@ -0,0 +1,15 @@
+package recursion;
+
+public class BuggyMethodTester {
+
+	public static void main(String[] args) {
+		System.out.println(buggyMethod(4));
+	}
+	
+	public static String buggyMethod(int n) {
+		if (n <= 0) {
+			return "";
+		}
+		return n + " " + buggyMethod(n-2);
+	}
+}
