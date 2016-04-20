a4a5f90db2aa50299820b01342ae420423e6eb52
jneethling-bbk <jneethling.bbk@gmail.com>
2016-04-20 17:08:40 +0100

    Added some basic syntax exercises

---
+++ Java_revision/src/parsing/DoubleParsing.java
@@ -0,0 +1,11 @@
+package parsing;
+
+public class DoubleParsing {
+
+	public static void main(String[] args) {
+		String firstString = "3.2";
+		String secondString = "2.3";
+		System.out.println(Double.parseDouble(firstString) + Double.parseDouble(secondString));
+	}
+
+}
---
+++ Java_revision/src/parsing/IntParsing.java
@@ -0,0 +1,12 @@
+package parsing;
+
+public class IntParsing {
+
+	public static void main(String[] args) {
+		String firstString = "3";
+		String secondString = "2";
+		System.out.println(Integer.parseInt(firstString) + Integer.parseInt(secondString));
+
+	}
+
+}
