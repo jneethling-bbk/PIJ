2dcda2bf2b48435de73e0040bef4ea8492dd43b5
jneethling-bbk <jneethling.bbk@gmail.com>
2016-04-21 18:34:18 +0100

    Another loop exercises

---
+++ Java_revision/src/loops/DoWhile.java
@@ -0,0 +1,12 @@
+package loops;
+
+public class DoWhile {
+
+	public static void main(String[] args) {
+		int myNumber = 0;
+		do {
+			System.out.println(myNumber);
+			myNumber++;
+		} while (myNumber < 10);
+	}
+}
