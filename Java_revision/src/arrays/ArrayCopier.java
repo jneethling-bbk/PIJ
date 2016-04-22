1f486b9aedd04a7fc7a0805282bfa8827f034801
jneethling-bbk <jneethling.bbk@gmail.com>
2016-04-22 13:44:55 +0100

    Added some array exercises

---
+++ Java_revision/src/arrays/ArrayCopier.java
@@ -0,0 +1,40 @@
+package arrays;
+
+public class ArrayCopier {
+
+	public static void main(String[] args) {
+		int[] src = {1, 2, 3, 4, 5};
+		int[] dst1 = new int[5];
+		int[] dst2 = new int[3];
+		int[] dst3 = new int[7];
+		arrayCopy(src, dst1);
+		arrayPrint(dst1);
+		arrayCopy(src, dst2);
+		arrayPrint(dst2);
+		arrayCopy(src, dst3);
+		arrayPrint(dst3);
+	}
+	static void arrayCopy(int[] src, int[] dst) {
+		if (src.length > dst.length) {
+			for (int i = 0; i < dst.length; i++) {
+				dst[i] = src[i];
+			}
+		} else {
+			for (int i = 0; i < src.length; i++) {
+				dst[i] = src[i];
+			}
+			if (src.length < dst.length) {
+				for (int i = dst.length - (dst.length-src.length); i < dst.length; i++) {
+					dst[i] = 0;
+				}
+			}
+		}
+	}
+	static void arrayPrint(int[] arr) {
+		for (int i = 0; i < arr.length; i++) {
+			System.out.print(arr[i]);
+		}
+		System.out.println("");
+	}
+
+}
