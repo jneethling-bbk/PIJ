b3d0b0af9f3f9facc546e82a74ff5af046ad97c2
jneethling-bbk <jneethling.bbk@gmail.com>
2016-04-26 18:20:28 +0100

    Added another method overloading exercise

--- Java_revision/src/methods/MethodOverloading.java
+++ Java_revision/src/methods/MethodOverloading.java
@@ -5,19 +5,23 @@ public class MethodOverloading {
 	public static void main(String[] args) {
 		System.out.println(calc(2, 3));
 		System.out.println(calc(2, 3, 4));
+		System.out.println(calc(2.4, 2.5));
 		System.out.println(calc("2", "3"));
 
 	}
-	static int calc(int a, int b) {
-		return   a + b;
+	static Number calc(int a, int b) {
+		return a + b;
 	}
 	
-	static int calc(int a, int b, int c) {
+	static Number calc(int a, int b, int c) {
 		return a + b + c;
 	}
 	
-	static int calc(String a, String b) {
+	static Number calc(double a, double b) {
+		return a + b;
+	}
+	
+	static Number calc(String a, String b) {
 		return Integer.parseInt(a) + Integer.parseInt(b);
 	}
-
 }
