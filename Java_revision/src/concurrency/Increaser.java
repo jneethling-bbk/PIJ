c885f88021829e3d715a4f16c71ba98dd198dc35
jneethling-bbk <jneethling.bbk@gmail.com>
2016-10-11 13:50:40 +0100

    Adapted code for programming test

--- Java_revision/src/concurrency/Increaser.java
+++ Java_revision/src/concurrency/Increaser.java
@@ -2,26 +2,28 @@ package concurrency;
 
 public class Increaser implements Runnable {
 
+	//Counter is a shared resource so numbers cannot be repeated
 	private Counter c;
 	
 	public Increaser(Counter c) {
 		this.c = c;
 	}
 	
-	@Override
-	public void run() {
-		System.out.println("Starting at " + c.getCount());
-		for (int i = 0; i < 1000; i++) {
-			c.increase();
-		}
-		System.out.println("Stopping at " + c.getCount());
-	}
-	
 	public static void main(String[] args) {
 		Counter counter = new Counter();
-		for (int i = 0; i < 100; i++) {
+		//There are 2 threads
+		for (int i = 0; i < 2; i++) {
 			Increaser task = new Increaser(counter);
 			new Thread(task).start();
+		}
+	}
+	
+	@Override
+	public void run() {
+		//Each thread will print 500 numbers
+		for (int i = 0; i < 500; i++) {
+			c.increase();
+			System.out.println(c.getCount());
 		}
 	}
 }
